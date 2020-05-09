b1957e484608730a01c85bbb2a26b99f5d0c30fa
TrungTran205 <tranquoctrung90@gmail.com>
2020-05-10 00:16:37 +0700

    First Commit
    First Commit

---
+++ Selenium016_TrungTQ/javafortester/javafortester/Test.java
@@ -0,0 +1,11 @@
+package javafortester;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+
+public class Test{
+	
+}
